90c770353021c5181d0044d6dd4336fe4a1bee70
Evan Travers <evantravers@gmail.com>
2014-01-21 10:02:46 -0600

    different color theme

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -73,7 +73,7 @@
                                           "%b"))))
 
 ;; use zenburn as the default theme
-(load-theme 'zenburn t)
+(load-theme 'soothe t)
 
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
